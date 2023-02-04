9633a80270f8a6b575050826cb7154e432adaea2
Dylan <dylanlee0613@gmail.com>
2023-02-04 10:03:41 -0800

    Added button functionality and placingTower

--- button.py
+++ button.py
@@ -1,5 +1,5 @@
 import pygame
-
+from colors import BLACK, WHITE
 class TowerButton:
     def __init__(self, towerName, Tower, x, y, width, height, color):
         self.towerName = towerName
@@ -10,12 +10,16 @@ class TowerButton:
         self.color = color
         self.rectangle = pygame.Rect(x - (1 / 2 * self.width), y - (1 / 2 * self.height), width, height)
         #Tower is class
-        self.tower = Tower(0, 0, 10, 0, x, y, splash = 0, neutral = True, placing = False)
+        self.towerWidth = 50
+        self.tower = Tower(0, 0, self.towerWidth, 0, x, y, splash = 0, neutral = True, placing = False)
 
 
     def render(self, screen):
+        font = pygame.font.SysFont("Arial", 10)
+        fontSurface = font.render(self.towerName, True, WHITE)
         pygame.draw.rect(screen, self.color, (self.x - (1 / 2 * self.width), self.y - (1 / 2 * self.height), self.width, self.height))
         self.tower.render(screen, None, "")
+        screen.blit(fontSurface, (self.x - (1/2 * fontSurface.get_width()), self.y + (1/2 * self.towerWidth)))
 
-    def ifClicked(self, x, y):
-        self.rectangle.collidepoint(x, y)
+    def isClicked(self, x, y):
+        return self.rectangle.collidepoint(x, y)
--- main.py
+++ main.py
@@ -10,8 +10,8 @@ pygame.init()
 
 #
 
-SCREENX = 400
-SCREENY = 400
+SCREENX = 800
+SCREENY = 800
 screen = pygame.display.set_mode((SCREENX, SCREENY))
 gamePath = Path([(0,0),(50, 0), (50, 50), (100, 50), (100,0)], 40, BLACK)
 testSlime = Slime(0.1, 10, gamePath, GREEN, 5, 11)
@@ -21,20 +21,31 @@ test = 0
 clock = pygame.time.Clock()
 frameRateTest = time.time()
 
-testButton = TowerButton("ArrowTower", ArrowTower, 200, 200, 20, 10, BLACK)
+testButton = TowerButton("ArrowTower", ArrowTower, 50, 750, 100, 100, BLACK)
 
 enemyList = [testSlime]
+towerList = []
+placingTower = None
 
 done = False
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                if testButton.isClicked(event.pos[0], event.pos[1]):
+                    print("Click")
+                    placingTower = ArrowTower(1, 1, 50, 100, event.pos[0], event.pos[1])
+        if event.type == pygame.MOUSEMOTION:
+            if placingTower:
+                placingTower.positionUpdate(event.pos[0], event.pos[1])
 
     screen.fill(WHITE)
 
     testButton.render(screen)
-
+    if placingTower:
+        placingTower.render(screen, gamePath, towerList)
     gamePath.render(screen)
 
     finishedEnemyList = []
@@ -50,7 +61,6 @@ while not done:
     if testPlayer.health <= 0:
         done = True
     clock.tick(60)
-    print(testPlayer.health)
 pygame.quit()
 
 
